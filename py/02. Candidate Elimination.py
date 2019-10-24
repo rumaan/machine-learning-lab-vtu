733dd78c1a1a6a4505a01d444e2fa4578614613c
monisha16 <monishatarkar16@gmail.com>
2019-10-25 01:04:00 +0530

    alt code for Candidate Elimination

--- py/02. Candidate Elimination.py
+++ py/02. Candidate Elimination.py
@@ -58,8 +58,67 @@ print('Final S: ', s_final, sep="\n")
 print('Final G: ', g_final, sep="\n")
 
 
-# In[ ]:
-
-
 data.head()
+
+__________________________________________________________________________________________________________________
+
+# Candidate-Elimination using csv-library
+
+import csv
+
+s=[0,0,0,0,0,0]
+g=["?","?","?","?","?","?"]
+p=["?","?","?","?","?","?"]
+
+def gen():
+	for k in range(1,len(i)-1):
+		if i[k]!=s[k-1]:
+			g[k-1]=s[k-1]
+
+
+with open("/home/monisha/machine-learning-lab-vtu/datasets/enjoysport.csv") as csvfile:
+	csvreader=csv.reader(csvfile)
+	for i in csvreader:
+		if i[6]=="yes":
+			if s[0] == 0:
+				s=i[:6]
+			else:
+				for k in range(0,len(i)-1):
+					if i[k] != s[k-1]:
+						s[k-1]="?"
+		else:
+			gen()
+	gen()
+
+print("specific hypothesis:",s)
+print("general hypothesis")
+for i ine range(0,leng(g)):
+	if g[i] !="?":
+		p[i]=g[i]
+		print(p)
+	p[i]="?"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
